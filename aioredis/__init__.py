aio-libs-abandoned/aioredis-py

Some redis sentinel WIP

aioredis/__init__.py
@@ -2,12 +2,16 @@ from .connection import RedisConnection, create_connection
 from .commands import Redis, create_redis, create_reconnecting_redis
 from .pool import RedisPool, create_pool
 from .util import Channel
+from .sentinel import RedisSentinel, create_sentinel
 from .errors import (
     RedisError,
     ProtocolError,
     ReplyError,
     PipelineError,
     MultiExecError,
+    ReadOnlyError,
+    MasterNotFoundError,
+    SlaveNotFoundError
     )
 
 
@@ -17,5 +21,7 @@ __version__ = '0.2.3'
 (create_connection, RedisConnection,
  create_redis, create_reconnecting_redis, Redis,
  create_pool, RedisPool, Channel,
+ create_sentinel, RedisSentinel,
  RedisError, ProtocolError, ReplyError,
- PipelineError, MultiExecError)
+ PipelineError, MultiExecError, MasterNotFoundError,
+ SlaveNotFoundError, ReadOnlyError)

ramjet-labs/aioredis

aioredis/__init__.py
@@ -2,13 +2,17 @@ from .connection import RedisConnection, create_connection
 from .commands import Redis, create_redis, create_reconnecting_redis
 from .pool import RedisPool, create_pool
 from .util import Channel
+from .sentinel import RedisSentinel, create_sentinel
 from .errors import (
     ConnectionClosedError,
+    MasterNotFoundError,
     MultiExecError,
     PipelineError,
     ProtocolError,
+    ReadOnlyError,
     RedisError,
     ReplyError,
+    SlaveNotFoundError,
     )
 
 
@@ -18,5 +22,8 @@ __version__ = '0.2.4'
 (create_connection, RedisConnection,
  create_redis, create_reconnecting_redis, Redis,
  create_pool, RedisPool, Channel,
+ create_sentinel, RedisSentinel,
  RedisError, ProtocolError, ReplyError,
- PipelineError, MultiExecError, ConnectionClosedError)
+ PipelineError, MultiExecError, ConnectionClosedError,
+ MasterNotFoundError, SlaveNotFoundError, ReadOnlyError,
+ )