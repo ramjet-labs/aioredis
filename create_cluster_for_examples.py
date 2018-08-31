6e1a1961d93ec6b272affeda6fecbffa39074241
Ashu Goel <ashu@brilliant.tech>
2018-08-31 15:50:14 -0700

    allow cluster to run forever for examples

--- create_cluster_for_examples.py
+++ create_cluster_for_examples.py
@@ -1,4 +1,14 @@
+import time
+
 from aioredis.cluster.testcluster import TestCluster
 
 cluster = TestCluster(list(range(7001, 7007)), '/tmp/rediscluster')
 cluster.setup()
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
+finally:
+    cluster.terminate()
+    cluster.clear_directories()
