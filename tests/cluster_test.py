243d4f7df84e8c7cba070e4c85a59bb4a4ed7f97
Ashu Goel <ashu@brilliant.tech>
2019-05-21 19:30:01 -0700

    [tests] fix cluster test

--- tests/cluster_test.py
+++ tests/cluster_test.py
@@ -2690,7 +2690,7 @@ async def test_transaction_raises_error_from_transaction(loop,
         mock.call(b"SET", second_key, "2"),
         mock.call("EXEC"),
     ])
-    assert "ReplyError('UH OH',)" in str(e)
+    assert "ReplyError('UH OH')" in str(e)
 
 
 @cluster_test
